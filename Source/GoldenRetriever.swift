c89d68008dac8ac34d6496d5cc0b192b52425c6e
Christoffer Winterkvist <chris@zenangst.com>
2015-06-17 16:58:59 +0200

    Add support for headers

--- Source/GoldenRetriever.swift
+++ Source/GoldenRetriever.swift
@@ -2,15 +2,36 @@ import Foundation
 
 public class GoldenRetriever {
 
+  var headers: [String : String]?
+
   public init() {}
 
+  public convenience init(headers: [String : String]) {
+    self.init()
+    
+    self.headers = headers
+  }
+
   public func fetch(resource: String, closure: (data: NSData, error: NSError?) -> Void) {
+    fetch(resource, closure: { (data, _, error) -> Void in
+      closure(data: data, error: error)
+    })
+  }
+
+  public func fetch(resource: String, closure: (data: NSData, response: NSURLResponse, error: NSError?) -> Void) {
     if let url = NSURL(string: resource) {
       let session = NSURLSession.sharedSession()
-      let request = NSURLRequest(URL: url)
+      let request = NSMutableURLRequest(URL: url)
+
+      if headers != nil {
+        for (key, value) in headers! {
+          request.addValue(value, forHTTPHeaderField: key)
+        }
+      }
+
       let task = session.dataTaskWithRequest(request,
         completionHandler: { (data, response, error) -> Void in
-          closure(data: data, error: error)
+          closure(data: data, response: response, error: error)
       })
 
       task.resume()
